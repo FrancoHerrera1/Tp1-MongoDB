fbe17056211d063fcf84a73b9603f2cf0258f9a6
¨FrancoHerrera1¨ <francocalf@gmail.com>
2025-06-07 14:22:58 -0300

    declared utils files

---
+++ src/utils/utilsBooks.ts
@@ -0,0 +1,19 @@
+const generos = [
+  "Fantasía",
+  "Ciencia ficción",
+  "Misterio",
+  "Romance",
+  "Terror",
+  "Thriller",
+  "Histórico",
+  "Aventura",
+  "Biografía",
+  "Autobiografía",
+  "Poesía",
+  "Drama",
+  "Comedia",
+  "No ficción",
+  "Autoayuda"
+];
+
+export { generos }
